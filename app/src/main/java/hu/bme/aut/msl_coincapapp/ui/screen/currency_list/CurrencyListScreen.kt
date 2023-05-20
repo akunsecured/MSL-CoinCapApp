6b5680c1ab8fbcb78a6878fc120a833574ea5835
akunsecured <tasi.adam@edu.bme.hu>
2023-05-20 12:10:48 +0200

    Adding SearchAppBar

--- app/src/main/java/hu/bme/aut/msl_coincapapp/ui/screen/currency_list/CurrencyListScreen.kt
+++ app/src/main/java/hu/bme/aut/msl_coincapapp/ui/screen/currency_list/CurrencyListScreen.kt
@@ -1,33 +1,52 @@
 package hu.bme.aut.msl_coincapapp.ui.screen.currency_list
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import hu.bme.aut.msl_coincapapp.model.Currency
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun CurrencyListScreen(
     navController: NavController,
@@ -35,12 +54,30 @@ fun CurrencyListScreen(
 ) {
     val currencyList by currencyListViewModel.currencyList.collectAsState(initial = emptyList())
     val isLoading by currencyListViewModel.isLoading
+    val isError by currencyListViewModel.isError
+
+    val searchWidgetState by currencyListViewModel.searchWidgetState
+    val searchTextState by currencyListViewModel.searchTextState
 
     Scaffold(
         topBar = {
-            TopAppBar(
-                modifier = Modifier.background(Color.Blue),
-                title = { Text("Currency list") })
+            CurrencyListAppBar(
+                searchWidgetState = searchWidgetState,
+                searchTextState = searchTextState,
+                onTextChange = {
+                    currencyListViewModel.updateSearchTextState(it)
+                },
+                onCloseClicked = {
+                    currencyListViewModel.updateSearchTextState("")
+                    currencyListViewModel.updateSearchWidgetState(SearchWidgetState.CLOSED)
+                },
+                onSearchClicked = {
+                    Log.d("SearchValue", it)
+                },
+                onSearchTriggered = {
+                    currencyListViewModel.updateSearchWidgetState(SearchWidgetState.OPENED)
+                }
+            )
         },
     ) { padding ->
         if (isLoading) {
@@ -52,24 +89,188 @@ fun CurrencyListScreen(
                 CircularProgressIndicator()
             }
         } else {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(padding),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                items(currencyList) { currency ->
-                    CurrencyItem(
-                        currency = currency,
-                        itemClick = { id ->
-                            navController.navigate("currency/$id")
-                        }
+            if (isError != null) {
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = isError!!,
+                        textAlign = TextAlign.Center
                     )
+                }
+            } else {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(padding),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    items(currencyList) { currency ->
+                        CurrencyItem(
+                            currency = currency,
+                            itemClick = { id ->
+                                navController.navigate("currency/$id")
+                            }
+                        )
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun CurrencyListAppBar(
+    searchWidgetState: SearchWidgetState,
+    searchTextState: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit,
+    onSearchTriggered: () -> Unit,
+) {
+    when (searchWidgetState) {
+        SearchWidgetState.CLOSED -> {
+            DefaultAppBar(onSearchTriggered)
+        }
+
+        SearchWidgetState.OPENED -> {
+            SearchAppBar(
+                text = searchTextState,
+                onTextChange = onTextChange,
+                onCloseClicked = onCloseClicked,
+                onSearchClicked = onSearchClicked
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DefaultAppBar(onSearchClicked: () -> Unit) {
+    TopAppBar(
+        modifier = Modifier.background(Color.Blue),
+        navigationIcon = {
+            IconButton(onClick = {
+
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                    contentDescription = "Icon"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                    contentDescription = "Search"
+                )
+            }
+        },
+        title = {
+            Text(
+                "Currency list",
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    )
+}
+
+@Composable
+@Preview
+fun DefaultAppBarPreview() {
+    DefaultAppBar {
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchAppBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = text,
+            onValueChange = onTextChange,
+            placeholder = {
+                Text(
+                    text = "Search here...",
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            },
+            textStyle = TextStyle(
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                color = MaterialTheme.colorScheme.onPrimary,
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search",
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        if (text.isNotEmpty()) {
+                            onTextChange("")
+                        } else {
+                            onCloseClicked()
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close",
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchClicked(text)
+                }
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Transparent
+            )
+        )
+    }
+}
+
+@Composable
+@Preview
+fun SearchAppBarPreview() {
+    SearchAppBar(
+        text = "Some random text",
+        onTextChange = {},
+        onCloseClicked = {},
+        onSearchClicked = {}
+    )
 }
 
 @Composable
@@ -93,4 +294,18 @@ fun CurrencyItem(
             Text(text = "Price: $${currency.priceUsd}")
         }
     }
+}
+
+@Composable
+@Preview
+fun CurrencyItemPreview() {
+    CurrencyItem(
+        currency = Currency(
+            id = "example",
+            rank = 1,
+            name = "Example-Coin",
+            symbol = "EC",
+            priceUsd = 1.23456
+        ),
+        itemClick = {})
 }
--- app/src/main/java/hu/bme/aut/msl_coincapapp/ui/screen/currency_list/CurrencyListViewModel.kt
+++ app/src/main/java/hu/bme/aut/msl_coincapapp/ui/screen/currency_list/CurrencyListViewModel.kt
@@ -8,12 +8,36 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+enum class SearchWidgetState {
+    OPENED,
+    CLOSED
+}
+
 @HiltViewModel
 class CurrencyListViewModel @Inject constructor(
     repository: CurrencyListRepository
 ) : ViewModel() {
     private val _isLoading: MutableState<Boolean> = mutableStateOf(false)
     val isLoading: State<Boolean> get() = _isLoading
+
+    private val _isError: MutableState<String?> = mutableStateOf(null)
+    val isError: State<String?> get() = _isError
+
+    private val _searchWidgetState: MutableState<SearchWidgetState> =
+        mutableStateOf(SearchWidgetState.CLOSED)
+    val searchWidgetState: State<SearchWidgetState> get() = _searchWidgetState
+
+    fun updateSearchWidgetState(newState: SearchWidgetState) {
+        _searchWidgetState.value = newState
+    }
+
+    private val _searchTextState: MutableState<String> =
+        mutableStateOf("")
+    val searchTextState: State<String> get() = _searchTextState
+
+    fun updateSearchTextState(newState: String) {
+        _searchTextState.value = newState
+    }
 
     val currencyList =
         repository.loadCurrencies(
@@ -22,8 +46,10 @@ class CurrencyListViewModel @Inject constructor(
             },
             onCompletion = {
                 _isLoading.value = false
+                _isError.value = null
             },
             onError = {
+                _isError.value = it
                 Log.e("CurrencyList", it)
             }
         )
